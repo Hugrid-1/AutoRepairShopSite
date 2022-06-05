a1a4b94f549aad85bb9df8885092f0d58a67e1c2
Vladimir <qer978@mail.ru>
2022-06-05 13:26:49 +0300

    changes

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+#подгрузка необходимых библиотек
 import datetime
+import re
+from typing import Union, Any
 
-from flask import Flask, render_template, redirect, url_for, request
-from flask_login import UserMixin, login_user, logout_user
-from flask_login import login_required, current_user, LoginManager
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_login import login_required, current_user, LoginManager,UserMixin, login_user, logout_user
 from sqlalchemy import create_engine
+from sqlalchemy.engine.mock import MockConnection
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
+
 db_string = "postgresql://vova:123@localhost/AutoRepairShop" #адрес подключения к БД
-
-
 
 
 app = Flask( __name__ ,static_folder='static',static_url_path='/static') #определение переменной приложения
@@ -16,7 +18,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = db_string
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'CVN5M974M12fgXda315sczNMx778JKMnb32cv'
 
-dbEngine = create_engine(db_string) #создание класса базы данных
+dbEngine: Union[MockConnection, Any] = create_engine(db_string) #создание класса базы данных
 dbApp = SQLAlchemy(app)
 
 login__manager = LoginManager(app)
@@ -24,7 +26,37 @@ LoginManager.login_view = 'authorization'
 LoginManager.login_message = "Авторизуйтесь для доступа к закрытым страницам"
 LoginManager.login_message_category = "success"
 
-autopartCart = []
+autopartCart = [] #список хранящий предметы в корзине
+
+
+def valid_password(password): # проверка пароля на корректность
+    checkStatus = False
+    if password is None:
+        return checkStatus
+    else:
+        if len(password) < 6: #Проверка длины пароля
+            print("Длина пароля недостаточна")
+            return False
+        alphaCounter = 0 #счетчик букв
+        for i in password:
+            if i.isalpha() and alphaCounter <= 2:
+                alphaCounter += 1 #увеличение счетчика
+            elif alphaCounter >= 3:
+                checkStatus = True
+                return checkStatus #выход из цикла при наборе нужного количества букв
+        print("Символов недостаточно")
+        return checkStatus
+
+def valid_telephone_number(inp): #проверка номера телефона на корректность
+    # проверка длины телефона, длина должна быть 12 и наличие индексов 3 и 7
+    # if not all(inp[x] == "-" for x in [7]) and len(inp) == 12:
+    #     return False
+    # return inp.replace("-", "", 3).isdigit()
+    return True
+
+def valid_fio(fio): #проверка корректности ФИО
+    return re.fullmatch(r'[А-ЯЁ][а-яё]+\s+[А-ЯЁ][а-яё]+(?:\s+[А-ЯЁ][а-яё]+)?', fio) #проверка соответствия регулярному выражению
+
 ### Работа с БД #######
 
 class User(dbApp.Model,UserMixin):
@@ -40,16 +72,22 @@ class User(dbApp.Model,UserMixin):
 ########ОБРАБОТКА АДРЕСНЫХ ПУТЕЙ###########
 
 #главная страница
-@app.route('/')
-@app.route('/home')
-def mainpage():
-    print()
+@app.route('/') # обработка 1-го варианта адресного пути
+@app.route('/home') # обработка 2-го варианта адресного пути
+def mainpage(): # функция
+    print("[LOG] Переход на главную страницу сайта")
     return render_template("index.html")
 
 #### ВЫВОД КАТАЛОГА АВТОЗАПЧАСТЕЙ ####
-@app.route('/catalog')
+@app.route('/catalog',methods=['GET','POST'])
 def showStampsList():
     data = dbEngine.execute('SELECT * FROM "Stamps"').fetchall()
+    minPrice = request.form.get('minPrice')
+    maxPrice = request.form.get('maxPrice')
+    if request.method == "POST":
+        print(minPrice,maxPrice)
+        # data = db
+
     return render_template("catalog.html",data=data,viewmode="stamp",autopartCart=autopartCart,cartLen=len(autopartCart))
 
 @app.route('/catalog/<stampId>')
@@ -64,45 +102,53 @@ def redirectToAutoPartList(stampID,modelID):
 
 @app.route('/catalog-for-your-choice/<modelID>',methods=['GET','POST'])
 def showAutoPartList(modelID):
-    data = dbEngine.execute(f'SELECT * FROM "Autoparts" WHERE id_model = {modelID}').fetchall()
+    data = dbEngine.execute(f'SELECT * FROM "Autoparts" WHERE id_model = {modelID}').fetchall() #запрос на получение запчастей по модели
     if request.method == "POST":
         if request.form['action'] == "addToCart":
             autopart = get_autopart(request.form['autopart'])
             autopartCart.append(autopart)
-            print("В корзину добавлена запчасть")
+            #print(f"[LOG] В корзину добавлена запчасть {autopart.modelName}")
             print(autopartCart)
+
         if request.form['action'] == "removeFromCart":
             autopart = get_autopart(request.form['autopart'])
-            print("Из корзины удалена запчасть")
+            # print(f"[LOG] Из корзины удалена запчасть {autopart.modelName}")
             autopartCart.remove(autopart)
             print(autopartCart)
+
     return render_template("catalog.html", data=data, viewmode="autopart", static='static',autopartCart=autopartCart,cartLen=len(autopartCart))
 
-
 def get_autopart(autopartID):
-
     autopart = dbEngine.execute(f'SELECT * FROM "Autoparts" WHERE id = {autopartID}').fetchone()
     return autopart
 
 @app.route('/cart',methods=['GET','POST'])
 @login_required
 def cartPage():
-    todayDate = datetime.date.today()
-    cartPrice = 0
-    for autopart in autopartCart:
+    todayDate = datetime.date.today() #получение сегодняшней даты
+    cartPrice = 0 #переменная для общей цены заказа
+    for autopart in autopartCart: #формирование цены заказа
         cartPrice += autopart.price
     if request.method == "POST":
         service_date = request.form['dateService']
         user_id = current_user.get_id()
         requestStatus = "Создан"
+        #добавление записи заказа
         dbEngine.execute(f'INSERT INTO "SellRequests" (create_day, event_day, user_id, status, cart_price) VALUES' + f"('{todayDate}', '{service_date}', {user_id},'{requestStatus}',{cartPrice}) RETURNING id")
         lastRow = dbEngine.execute('SELECT * FROM public."SellRequests"ORDER BY id DESC ')
-        request_id = lastRow.fetchone().id
+        request_id = lastRow.fetchone().id # получение id только что добавленной записи
+
+        #добавление записей в таблицу содержимого заказов
         for autopart in autopartCart:
             dbEngine.execute(f'INSERT INTO "Sell_items" (request_id,autopart_id) VALUES' + f"('{request_id}', '{autopart.id}')")
-    return render_template("buy.html",autopartCart=autopartCart,cartLen=len(autopartCart),CartPrice=cartPrice)
 
+        print(f"[LOG] СОЗДАН ЗАКАЗ С ID:{request_id} , пользователем {current_user.login} id:{current_user.get_id()}")
+        autopartCart.clear() #очистка корзины после создания заказа
+        print("[LOG] Корзина очищена")
 
+        return redirect(url_for('showAccountInfo')) #переадресация в профиль
+
+    return render_template("buy.html",todayDate=todayDate,autopartCart=autopartCart,cartLen=len(autopartCart),CartPrice=cartPrice)
 
 @app.route('/services')
 def showServiceList():
@@ -110,7 +156,6 @@ def showServiceList():
     return render_template("services.html",services=serviceList)
 
 @app.route('/serviceRegistration/<service_id>',methods=['GET','POST'])
-@login_required
 def addServiceRequest(service_id):
     todayDate = datetime.date.today()
     service = dbEngine.execute(f'SELECT * FROM "Services" WHERE id = {service_id}').fetchone()
@@ -118,9 +163,15 @@ def addServiceRequest(service_id):
         print(request.form)
         service_date = request.form['dateService']
         user_id = current_user.get_id()
+        type(user_id)
+        if user_id is None:
+            return redirect(url_for('authorization'))
         requestStatus = "Создан"
-        dbEngine.execute(f'INSERT INTO "ServiceRequests" (create_day, event_day, service_id, user_id, status) VALUES' + f"('{todayDate}', '{service_date}', {service_id},{user_id},'{requestStatus}')")
+        print(service.id)
+        dbEngine.execute(f'INSERT INTO "ServiceRequests" (service_id,create_day, event_day,  user_id, status)  VALUES' + f"({service.id},'{todayDate}', '{service_date}', {user_id},'{requestStatus}') RETURNING id")
 
+        print(f"[LOG] ЗАПИСЬ НА УСЛУГУ {service.serviceName} пользователем {current_user.login} id:{current_user.get_id()}")
+        return redirect(url_for('showAccountInfo'))  # переадресация в профиль
     return render_template("ServiceReg.html",service=service,todayDate=todayDate)
 
 
@@ -138,7 +189,6 @@ def showAccountInfo():
     print(current_user.login,current_user.id,current_user.fio)
     userServiceRequestList = dbEngine.execute(f'SELECT * FROM "UserServiceRequests"'+f" WHERE user_id = {current_user.id} ")
     userSellRequestList =  dbEngine.execute(f'SELECT * FROM "SellRequests"'+f" WHERE user_id = {current_user.id} ")
-    #buy_history
     return render_template("accountPage.html",userServiceRequestList=userServiceRequestList,userSellRequestList=userSellRequestList)
 
 #Авторизация/Регистрация пользователя
@@ -148,19 +198,30 @@ def authorization():
     if request.method =="POST":
         if request.form["checkForm"] == "registration":
             #получение данных из формы регистрации
-            username = request.form["username"]
-            password = request.form["password"]
-            email = request.form["email"]
-            fio = request.form["fio"]
-            telephone = request.form["telephone"]
+            username = request.form.get("username")
+            password = request.form.get("password")
+            retry_password = request.form.get("retry_password")
+            email = request.form.get("email")
+            fio = request.form.get("fio")
+            telephone = request.form.get("telephone")
 
             hash_pwd = generate_password_hash(password) #кодировка пароля
 
             if username and password and email and fio and telephone:
-                dbEngine.execute(f'INSERT INTO "Users" (login, email, fio, password, telephone) VALUES' + f"('{username}', '{email}', '{fio}','{hash_pwd}','{telephone}')")
-                return redirect(request.args.get("next") or url_for("showAccountInfo"))
+                if password != retry_password:
+                    flash('Пароли не одинаковы')
+                    # return render_template("authorization.html")
+                elif not valid_password(password):
+                    flash('Пароль некорректен')
+                    # return render_template("authorization.html")
+                elif not valid_fio(fio):
+                    flash('ФИО некорректно')
+                    # return render_template("authorization.html")
+                else:
+                    dbEngine.execute(f'INSERT INTO "Users" (login, email, fio, password, telephone) VALUES' + f"('{username}', '{email}', '{fio}','{hash_pwd}','{telephone}')")
+                    return redirect(request.args.get("next") or url_for("showAccountInfo"))
             else:
-                print("Заполнены не все поля")
+                flash("Заполнены не все поля")
         elif request.form["checkForm"] == "login":
             login = request.form["username"]
             password = request.form["password"]
@@ -173,7 +234,7 @@ def authorization():
                     print(f"АВТОРИЗОВАН {current_user.get_id()}")
                     return redirect(request.args.get("next") or url_for("showAccountInfo"))
                 else:
-                    print("ПОЛЬЗОВАТЕЛЬ НЕ СУЩЕСТВУЕТ ЛИБО ПАРОЛЬ НЕВЕРНЫЙ")
+                    print("[LOG ERROR] ПОЛЬЗОВАТЕЛЬ НЕ СУЩЕСТВУЕТ ЛИБО ПАРОЛЬ НЕВЕРНЫЙ")
             else:
                 pass #ПРОПИСАТЬ
     return render_template("authorization.html")
@@ -194,7 +255,18 @@ def unauthorized():
 @login_required
 def logout():
     logout_user()
+    print("[LOG] ПОЛЬЗОВАТЕЛЬ ВЫШЕЛ ИЗ АККАУТНА")
     return redirect(url_for('mainpage'))
+
+@app.errorhandler(404)
+def not_found_error(error):
+    print('[ОШИБКА 404]',error)
+    return render_template('index.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    print('[ОШИБКА 500]',error)
+    return render_template('index.html'), 500
 
 dbApp.create_all()
 app.run(debug=True) #запуск программы на локальном сервере
